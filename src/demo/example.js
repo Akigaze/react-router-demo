c0978081dbc9c080b190f47bd96a318615dff5ab
Akigaze <1175520196@qq.com>
2019-11-04 21:13:01 +0800

    add url param example

--- src/demo/example.js
+++ src/demo/example.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
-export default function Example() {
-  return <></>;
+export default function ParamsExample() {
+  return (
+    <BrowserRouter>
+      <h2>Account</h2>
+      <ul>
+        <li>
+          <Link to="/netflix">Netflix</Link>
+        </li>
+        <li>
+          <Link to="/zillow-group">Zillow Group</Link>
+        </li>
+        <li>
+          <Link to="/yahoo">Yahoo</Link>
+        </li>
+        <li>
+          <Link to="/modus-create">Modus Create</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route path="/:id" children={({ match }) => <Child {...match} />} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function Child({ params }) {
+  return <h3>ID: {params.id}</h3>;
 }

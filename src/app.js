c29504bccc4189eda628679bbd48846a40088288
Akigaze <1175520196@qq.com>
2019-11-04 21:16:22 +0800

    add example template

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import "./style/app.css";
+import Example from "./demo/example";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render(){
-        return (
-            <div>
-                <h1 className="hurray">Hello React!</h1>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello React Router!</h1>
+        <Example/>
+      </div>
+    );
+  }
 }

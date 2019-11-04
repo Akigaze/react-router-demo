de8bcd98c3a35eb285f9efcba8b2f5ff3edf2895
Akigaze <1175520196@qq.com>
2019-11-04 20:58:30 +0800

    add basic example

--- src/demo/example.js
+++ src/demo/example.js
@@ -1,5 +1,44 @@
 import React from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
-export default function Example() {
-  return <></>;
+export default function BasicExample() {
+  return (
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
 }

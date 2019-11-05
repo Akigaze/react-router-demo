4a65eecbdff659b05c1d236b031e8612b89b98d4
Akigaze <1175520196@qq.com>
2019-11-05 22:55:30 +0800

    add not match 404 navigation example

--- src/demo/example.js
+++ src/demo/example.js
@@ -1,5 +1,55 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 
-export default function Example() {
-  return <></>;
+export default function NoMatchExample() {
+  return (
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/old-match">Old Match, to be redirected</Link>
+        </li>
+        <li>
+          <Link to="/will-match">Will Match</Link>
+        </li>
+        <li>
+          <Link to="/will-not-match">Will Not Match</Link>
+        </li>
+        <li>
+          <Link to="/also/will/not/match">Also Will Not Match</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/old-match">
+          <Redirect to="/will-match" />
+        </Route>
+        <Route path="/will-match">
+          <WillMatch />
+        </Route>
+        <Route path="*" component={({ match }) => <NotMatch {...match} />} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function WillMatch() {
+  return <h2>Matched!</h2>;
+}
+
+function NotMatch({ url }) {
+  return (
+    <div>
+      <h2 style={{ display: "inline-block" }}>No match for</h2>{" "}
+      <code>{url}</code>
+    </div>
+  );
 }

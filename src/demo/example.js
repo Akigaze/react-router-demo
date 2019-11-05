bcbafd1c0e6f1daa7abd617099c17b786d88f2ce
Akigaze <1175520196@qq.com>
2019-11-05 20:50:22 +0800

    add custom link example

--- src/demo/example.js
+++ src/demo/example.js
@@ -1,5 +1,53 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  matchPath
+} from "react-router-dom";
 
-export default function Example() {
-  return <></>;
+export default function CustomLinkExample() {
+  return (
+    <BrowserRouter>
+      <Route
+        path="/"
+        component={({ location }) => (
+          <div>
+            <OldSchoolMenuLink exact location={location} label="Home" to="/" />
+            <OldSchoolMenuLink location={location} label="About" to="/about" />
+          </div>
+        )}
+      />
+      <hr />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function OldSchoolMenuLink({ location, label, exact, to, ...reset }) {
+  const match = matchPath(location.pathname, { path: to, exact });
+  return (
+    <div>
+      {match && "> "}
+      <Link to={to} {...reset}>
+        {label}
+      </Link>
+    </div>
+  );
 }
